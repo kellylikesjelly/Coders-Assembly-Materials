ce5a08ddcf47d02cac2eb3b77f78d3d108d91110
kellylikesjelly <70317445+kellylikesjelly@users.noreply.github.com>
2020-09-18 09:13:24 +0800

    Update q3.py

--- 5_Loops/while_loops/q3.py
+++ 5_Loops/while_loops/q3.py
@@ -1,6 +1,13 @@
 
 def calculate_average_length(sentences):
-    pass
+    if len(sentences)==0:
+        return 0.0
+    sum_sentence=0
+    for sentence in sentences:
+        length_of_sentence=len(sentence)
+        sum_sentence=sum_sentence+length_of_sentence #right side evaluated/calculated first
+    return sum_sentence/len(sentences)
+        
 
 
 
